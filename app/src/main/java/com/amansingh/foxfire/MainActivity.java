175d0a2ab7883d7621f93e5ebf847f08f2d0ad56
Ranjeet_kumar <kumarranjeet90903@gmail.com>
2019-05-18 17:02:29 +0530

    Geofencing,create Constant GeofencingRegistrationService class, create Geafencing Related mathod

--- app/src/main/java/com/amansingh/foxfire/MainActivity.java
+++ app/src/main/java/com/amansingh/foxfire/MainActivity.java
@@ -1,14 +1,22 @@
 package com.amansingh.foxfire;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.location.Location;
 import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -19,12 +27,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.amansingh.foxfire.Utils.Utils;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -32,6 +46,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -67,6 +83,44 @@ public class MainActivity extends FragmentActivity
     private MediaRecorder myAudioRecorder;
     private String outputFile;
 
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_LOCATION_PERMISSION_CODE = 101;
+    private GeofencingRequest geofencingRequest;
+    private boolean isMonitoring = false;
+    private MarkerOptions markerOptions;
+    private PendingIntent pendingIntent;
+
+    public static double getSpeed(Location currentLocation, Location oldLocation) {
+        //  Click Speed of maps
+        //TODO: 16/5/19 do Notification
+
+        double newLat = currentLocation.getLatitude();
+        double newLon = currentLocation.getLongitude();
+
+        double oldLat = oldLocation.getLatitude();
+        double oldLon = oldLocation.getLongitude();
+
+        if (currentLocation.hasSpeed()) {
+            return currentLocation.getSpeed();
+        } else {
+            double radius = 6371000;
+            double dLat = Math.toRadians(newLat - oldLat);
+            double dLon = Math.toRadians(newLon - oldLon);
+            double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                    Math.cos(Math.toRadians(newLat)) * Math.cos(Math.toRadians(oldLat)) *
+                            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+            double c = 2 * Math.asin(Math.sqrt(a));
+            double distance = Math.round(radius * c);
+
+            double timeDifferent = currentLocation.getTime() - oldLocation.getTime();
+            return distance / timeDifferent;
+        }
+    }
+
+
+    private void search() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,19 +133,46 @@ public class MainActivity extends FragmentActivity
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).build();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION_PERMISSION_CODE);
+        }
+
+
         searchET.setVisibility(View.GONE);
 
         searchET.setOnFocusChangeListener((v, hasFocus) -> {
             if (!hasFocus)
                 searchET.setVisibility(View.GONE);
         });
-        mapSpeedTV.setOnClickListener(view -> {
-            screenShot();
-        });
+
     }
 
+    private void addNotification() {
 
-    private void search() {
+        //click Notification on Maps
+        // TODO: 16/5/19 do Notification
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Notification Example")
+                .setContentText("This is a best Notification");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(contentPendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+
 
     }
 
@@ -122,6 +203,153 @@ public class MainActivity extends FragmentActivity
             Log.e("GREC", e.getMessage(), e);
         }
     }
+
+    private void startGeofencing() {
+        Log.d(TAG, "Start geofencing monitoring call");
+        pendingIntent = getGeofencePendingIntent();
+        geofencingRequest = new GeofencingRequest.Builder()
+                .setInitialTrigger(Geofence.GEOFENCE_TRANSITION_ENTER)
+                .addGeofence(getGeofence())
+                .build();
+
+        if (!mGoogleApiClient.isConnected()) {
+            Log.d(TAG, "Google API client not connected");
+        } else {
+            try {
+                LocationServices.GeofencingApi.addGeofences(mGoogleApiClient, geofencingRequest,
+                        pendingIntent).setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        if (status.isSuccess()) {
+                            Log.d(TAG, "Successfully Geofencing Connected");
+                        } else {
+                            Log.d(TAG, "Failed to add Geofencing " + status.getStatus());
+                        }
+                    }
+                });
+            } catch (SecurityException e) {
+                Log.d(TAG, e.getMessage());
+            }
+        }
+        isMonitoring = true;
+        invalidateOptionsMenu();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+
+        if (pendingIntent != null) {
+            return pendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceRegistrationService.class);
+        return PendingIntent.getService(this, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+    }
+
+    private Geofence getGeofence() {
+        // get Geofencing click..
+
+        LatLng latLng = Constant.AREA_LANDMARKS.get(Constant.GEOFENCE_ID_STAN_UNI);
+        return new Geofence.Builder()
+                .setRequestId(Constant.GEOFENCE_ID_STAN_UNI)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .setCircularRegion(latLng.latitude, latLng.longitude, Constant.GEOFENCE_RADIUS_IN_METERS)
+                .setNotificationResponsiveness(1000)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build();
+    }
+
+    private void stopGeoFencing() {
+        pendingIntent = getGeofencePendingIntent();
+        LocationServices.GeofencingApi.removeGeofences(mGoogleApiClient, pendingIntent)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        if (status.isSuccess())
+                            Log.d(TAG, "Stop geofencing");
+                        else
+                            Log.d(TAG, "Not stop geofencing");
+                    }
+                });
+        isMonitoring = false;
+        invalidateOptionsMenu();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int response = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if (response != ConnectionResult.SUCCESS) {
+            Log.d(TAG, "Google Play Service Not Available");
+            GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, response, 1).show();
+        } else {
+            Log.d(TAG, "Google play service available");
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.reconnect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.manu_map_activity, menu);
+        if (isMonitoring) {
+            menu.findItem(R.id.action_start_monitor).setVisible(false);
+            menu.findItem(R.id.action_stop_monitor).setVisible(true);
+        } else {
+            menu.findItem(R.id.action_start_monitor).setVisible(true);
+            menu.findItem(R.id.action_stop_monitor).setVisible(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_start_monitor:
+                startGeofencing();
+                break;
+            case R.id.action_stop_monitor:
+                stopGeoFencing();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void locationGeofencing() {
+
+        LatLng latLng = Constant.AREA_LANDMARKS.get(Constant.GEOFENCE_ID_STAN_UNI);
+        map.addMarker(new MarkerOptions().position(latLng).title("Stanford University"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f));
+
+
+        Circle circle = map.addCircle(new CircleOptions()
+                .center(new LatLng(latLng.latitude, latLng.longitude))
+                .radius(Constant.GEOFENCE_RADIUS_IN_METERS)
+                .strokeColor(Color.RED)
+                .strokeWidth(4f));
+    }
+
+
+
+
+
+
+
 
     private void audioRecoding() {
         init();
@@ -174,9 +402,13 @@ public class MainActivity extends FragmentActivity
         super.onPause();
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+        locationGeofencing();
+        map.setMyLocationEnabled(true);
+
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         //Initialize Google Play Services
@@ -186,7 +418,10 @@ public class MainActivity extends FragmentActivity
                     == PackageManager.PERMISSION_GRANTED) {
                 //Location Permission already granted
                 buildGoogleApiClient();
+
                 map.setMyLocationEnabled(true);
+
+
             } else {
                 //Request Location Permission
                 checkLocationPermission();
@@ -194,8 +429,10 @@ public class MainActivity extends FragmentActivity
         } else {
             buildGoogleApiClient();
             map.setMyLocationEnabled(true);
+
         }
     }
+
 
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -208,7 +445,7 @@ public class MainActivity extends FragmentActivity
 
     @Override
     public void onLocationChanged(Location location) {
-        //when location changes
+        //when location changes Edit.........
         if (mCurrLocationMarker != null) {
             mCurrLocationMarker.remove();
         }
@@ -227,6 +464,7 @@ public class MainActivity extends FragmentActivity
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        //Edit.........
         LocationRequest mLocationRequest = LocationRequest.create();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         mLocationRequest.setInterval(5000);
@@ -235,16 +473,55 @@ public class MainActivity extends FragmentActivity
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+            startLocationMonitor();
+            startGeofencing();
         }
+    }
+
+    private void startLocationMonitor() {
+
+        //Edit.........
+
+        Log.d(TAG, "start location monitor");
+        LocationRequest locationRequest = LocationRequest.create()
+                .setInterval(2000)
+                .setFastestInterval(1000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        try {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+
+                    if (mCurrLocationMarker != null) {
+                        mCurrLocationMarker.remove();
+                    }
+                    markerOptions = new MarkerOptions();
+                    markerOptions.position(new LatLng(location.getLatitude(), location.getLongitude()));
+                    markerOptions.title("Current Location");
+                    mCurrLocationMarker = map.addMarker(markerOptions);
+                    Log.d(TAG, "Location Change Lat Lng " + location.getLatitude() + " " + location.getLongitude());
+                }
+            });
+        } catch (SecurityException e) {
+            Log.d(TAG, e.getMessage());
+        }
+
+
     }
 
     @Override
     public void onConnectionSuspended(int i) {
+        //Edit.........
+        Log.d(TAG, "Google Connection Suspended");
 
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        //Edit.........
+        isMonitoring = false;
+        Log.e(TAG, "Connection Failed:" + connectionResult.getErrorMessage());
 
     }
 
